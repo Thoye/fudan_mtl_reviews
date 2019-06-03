76edee2d718b6edee47f7fee7c5d40a8f4bafe7c
Thoye <37924652+Thoye@users.noreply.github.com>
2019-06-03 21:37:14 +0800

    Update mtl_model.py

--- src/models/mtl_model.py
+++ src/models/mtl_model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from models.base_model import * 
+from src.models.base_model import *
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -23,17 +23,17 @@ class MTLModel(BaseModel):
                                       trainable=w_trainable)
     
     self.shared_conv = ConvLayer('conv_shared', FILTER_SIZES)
-    self.shared_linear = LinearLayer('linear_shared', TASK_NUM, True)
+    self.shared_linear = LinearLayer('linear_shared', TASK_NUM, True)  # 'linear_shared'是多分类
 
     self.tensors = []
 
     for task_name, data in all_data:
-      with tf.name_scope(task_name):
+      with tf.name_scope(task_name):  # task_name: task_m, task_n
         self.build_task_graph(data)
 
-  def adversarial_loss(self, feature, task_label):
+  def adversarial_loss(self, feature, task_label):  # loss_adv
     '''make the task classifier cannot reliably predict the task based on 
-    the shared feature
+    the shared feature   :使分类器无法区分
     '''
     # input = tf.stop_gradient(input)
     feature = flip_gradient(feature)
@@ -41,73 +41,73 @@ class MTLModel(BaseModel):
       feature = tf.nn.dropout(feature, FLAGS.keep_prob)
 
     # Map the features to TASK_NUM classes
-    logits, loss_l2 = self.shared_linear(feature)
+    logits, loss_adv_l2 = self.shared_linear(feature)  # loss_adv_l2 是'linear_shared'层的w和b 的loss; 原来是多分类
 
-    label = tf.one_hot(task_label, TASK_NUM)
+    label = tf.one_hot(task_label, TASK_NUM)  # 多分类
     loss_adv = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=logits))
 
-    return loss_adv, loss_l2
+    return loss_adv, loss_adv_l2  # loss_adv 是多分类的交叉熵; loss_adv_l2是'linear_shared'层的w和b 的loss
   
-  def diff_loss(self, shared_feat, task_feat):
+  def diff_loss(self, shared_feat, task_feat):  # 正交化约束
     '''Orthogonality Constraints from https://github.com/tensorflow/models,
     in directory research/domain_adaptation
     '''
-    task_feat -= tf.reduce_mean(task_feat, 0)
-    shared_feat -= tf.reduce_mean(shared_feat, 0)
+    task_feat -= tf.reduce_mean(task_feat, 0)  # 私有层 conv_out
+    shared_feat -= tf.reduce_mean(shared_feat, 0)  # 共享层 shared_out
 
-    task_feat = tf.nn.l2_normalize(task_feat, 1)
-    shared_feat = tf.nn.l2_normalize(shared_feat, 1)
+    task_feat = tf.nn.l2_normalize(task_feat, 1)  # 不重要
+    shared_feat = tf.nn.l2_normalize(shared_feat, 1)  # 不重要
 
-    correlation_matrix = tf.matmul(
+    correlation_matrix = tf.matmul(  # matmul 乘法
         task_feat, shared_feat, transpose_a=True)
 
-    cost = tf.reduce_mean(tf.square(correlation_matrix)) * 0.01
-    cost = tf.where(cost > 0, cost, 0, name='value')
+    cost = tf.reduce_mean(tf.square(correlation_matrix)) * 0.01  # cost: 平方再求平均
+    cost = tf.where(cost > 0, cost, 0, name='value')  # cost = max(0,cost)
 
     assert_op = tf.Assert(tf.is_finite(cost), [cost])
     with tf.control_dependencies([assert_op]):
       loss_diff = tf.identity(cost)
 
-    return loss_diff
+    return loss_diff  # conv_out, shared_out 先乘后平方再求平均, 即为正交化约束loss
 
   def build_task_graph(self, data):
     task_label, labels, sentence = data
-    sentence = tf.nn.embedding_lookup(self.word_embed, sentence)
+    sentence = tf.nn.embedding_lookup(self.word_embed, sentence)  # tf.nn.embedding_lookup得到句子中每个word的embedding
 
     if self.is_train:
       sentence = tf.nn.dropout(sentence, FLAGS.keep_prob)
     
-    conv_layer = ConvLayer('conv_task', FILTER_SIZES)
-    conv_out = conv_layer(sentence)
-    conv_out = max_pool(conv_out, 500)
+    conv_layer = ConvLayer('conv_task', FILTER_SIZES)  # 'conv_task'是个layer_name
+    conv_out = conv_layer(sentence)  # 调用ConvLayer.call 返回一个conv_outs=[conv1,conv2,...]
+    conv_out = max_pool(conv_out, 500)  # conv_out 私有层 []
 
-    shared_out = self.shared_conv(sentence)
-    shared_out = max_pool(shared_out, 500)
+    shared_out = self.shared_conv(sentence)  # self.shared_conv = ConvLayer('conv_shared', FILTER_SIZES)
+    shared_out = max_pool(shared_out, 500)  # shared_out 共享层 []
 
     if self.adv:
-      feature = tf.concat([conv_out, shared_out], axis=1)
+      feature = tf.concat([conv_out, shared_out], axis=1)  # 加对抗 feature = concat([私有层 + 共享层])
     else:
-      feature = conv_out
+      feature = conv_out  # 不加对抗
 
-    if self.is_train:
+    if self.is_train:  # 无所谓，加上dropout
       feature = tf.nn.dropout(feature, FLAGS.keep_prob)
 
     # Map the features to 2 classes
-    linear = LinearLayer('linear', 2, True)
-    logits, loss_l2 = linear(feature)
+    linear = LinearLayer('linear', 2, True)  # 'linear'是个layer_name; 原来是二分类
+    logits, loss_l2 = linear(feature)  # logits = o = xw_plus_b 全连接; loss_l2 是'linear'层的w和b 的loss; 二分类
     
     xentropy = tf.nn.softmax_cross_entropy_with_logits(
-                          labels=tf.one_hot(labels, 2), 
+                          labels=tf.one_hot(labels, 2),  # labels 从 data 来的
                           logits=logits)
-    loss_ce = tf.reduce_mean(xentropy)
+    loss_ce = tf.reduce_mean(xentropy)  # loss_ce: loss_task 交叉熵loss
 
-    loss_adv, loss_adv_l2 = self.adversarial_loss(shared_out, task_label)
-    loss_diff = self.diff_loss(shared_out, conv_out)
+    loss_adv, loss_adv_l2 = self.adversarial_loss(shared_out, task_label)  # loss_adv; (loss_adv_l2:'linear_shared'层的)
+    loss_diff = self.diff_loss(shared_out, conv_out)  # loss_diff 正交化约束loss
 
-    if self.adv:
+    if self.adv:  # 加对抗需要加 loss_adv; (loss_adv_l2:w和b 的loss); loss_diff
       loss = loss_ce + 0.05*loss_adv + FLAGS.l2_coef*(loss_l2+loss_adv_l2) + loss_diff
-    else:
+    else:  # 不加对抗只需要加 loss_ce; (loss_l2:w和b 的loss)
       loss = loss_ce  + FLAGS.l2_coef*loss_l2
     
     pred = tf.argmax(logits, axis=1)
@@ -116,7 +116,10 @@ class MTLModel(BaseModel):
 
     self.tensors.append((acc, loss))
     # self.tensors.append((acc, loss, pred))
-    
+
+# ---------------------以上是重要部分-------------------------------------------------------------------------------
+# ---------------------以下不重要----------------------------------------------------------------------------------
+
   def build_train_op(self):
     if self.is_train:
       self.train_ops = []
